2038cef5ac100e78ff9eb68fd0f96d0d096bbffc
Louis Gesbert <louis.gesbert@ocamlpro.com>
2012-11-13 17:59:44 +0100

    test and fix for issue #6: indentation after let-open

--- src/block.ml
+++ src/block.ml
@@ -347,7 +347,6 @@ let rec update_path t stream tok =
 
   match tok.token with
     | SEMISEMI    -> append KNone L 0 (unwind_top t.path)
-    | OPEN        -> append KOpen L 2 (unwind_top t.path)
     | INCLUDE     -> append KInclude L 2 (unwind_top t.path)
     | EXCEPTION   -> append KException L 2 (unwind_top t.path)
     | BEGIN       -> append KBegin L 2 t.path
@@ -364,6 +363,10 @@ let rec update_path t stream tok =
     | STRUCT      -> append KStruct L 2 t.path
     | WHEN        -> append KWhen L 4 t.path
     | SIG         -> append KSig L 2 t.path
+
+    | OPEN when last_token t = Some LET -> append KOpen L 2 t.path
+
+    | OPEN -> append KOpen L 2 (unwind_top t.path)
 
     | LET when close_top_let t.last ->
         append KLet L 4 (unwind_top t.path)
