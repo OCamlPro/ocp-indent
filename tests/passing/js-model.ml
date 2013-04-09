5f8ba9ba4c9f150bc0c5b46b31b46f5686835002
Peter Szilagyi <pszilagyi@janestreet.com>
2013-04-09 09:30:15 -0400

    more JaneStreet regression tests

---
+++ tests/passing/js-model.ml
@@ -0,0 +1,28 @@
+
+val f
+  :  int
+  -> int
+
+type t =
+  | A
+  | B
+
+let height = function
+  | A -> 0
+  | B -> 1
+
+let _ =
+  if x then begin
+    y
+  end else if x then
+    y
+  else z
+
+let _ =
+  if x then y else
+  if x then y else
+    x
+
+type t
+  =  int
+  -> int
