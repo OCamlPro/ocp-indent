ba519e38c4d803287a915f24bef1a44273dd7646
Peter Szilagyi <pszilagyi@janestreet.com>
2013-04-09 15:12:16 -0400

    more JaneStreet cases with deeper nesting and operator alignment

--- tests/failing/js-args.ml
+++ tests/failing/js-args.ml
@@ -83,6 +83,12 @@ let () =
   raise (Bug ("foo"
               (* In this and similar cases, we want the subsequent lines to
                  align with the first expression. *)
+              ^ "bar"));
+  raise (Bug ("foo" ^ "quux"
+              ^ "bar"))
+  raise (Bug (foo + quux
+              ^ "bar"))
+  raise (Bug ((foo + quux)
               ^ "bar"))
 
 (* Except in specific cases, we want the argument indented relative to the
@@ -91,3 +97,57 @@ let () =
 let () =
   Some (Message_store.create s
           "herd-retransmitter" ~unlink:true Message_store.Message_size.Byte)
+
+
+
+(* We like the indentation of most arguments, but want to get back towards the
+   left margin in a few special cases: *)
+let _ =
+  foo (bar (fun x ->                    (* special: "fun _ ->" at EOL *)
+    baz))                               (* assume no more arguments *)
+let _ =
+  foo
+    ~a_long_field_name:(check (fun bar ->
+      baz))
+let _ =
+  foo ~a_long_field_name:(check (fun bar ->
+    baz))
+let _ =
+  foo (bar (quux (fnord (fun x ->       (* any depth *)
+    baz))))
+
+(* We also wanted to tweak the operator indentation, making operators like <=
+   not special cases in contexts like this:  *)
+let _ =
+  assert (foo (bar + baz
+               <= quux))                (* lined up under left argument to op,
+                                           sim. to ^ above *)
+let _ =
+  let min_closing_backoff =
+    -. (   Hidden_float.expose (arb.cfg.base_edge @! Buy)
+        +. Hidden_float.expose (arb.cfg.base_edge @! Sell))
+  in
+  0
+
+(* Sim. indentation of if conditions: *)
+let _ =
+  if (a
+      <= b)
+  then ()
+let _ =
+  if a
+     <= b
+  then ()
+let _ =
+  if Edge_adjustment.is_zero arb.cfg.extra_edge
+     && 0. = sys.plugs.edge_backoff
+     && 0. = zero_acvol_edge_backoff
+  then 0.
+  else 1.
+let _ =
+  if
+    Edge_adjustment.is_zero arb.cfg.extra_edge
+    && 0. = sys.plugs.edge_backoff
+    && 0. = zero_acvol_edge_backoff
+  then 0.
+  else 1.
