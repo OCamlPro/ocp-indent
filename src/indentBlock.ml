d85dc5580a38092efa43bd744f803bf58eedc690
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-04-10 16:33:29 +0200

    No extra indent after '(struct\n' either

--- src/indentBlock.ml
+++ src/indentBlock.ml
@@ -659,7 +659,8 @@ let rec update_path config block stream tok =
            replace KFun L (unwind (function KFun -> true | _ -> false) p)
        | p -> append KFun L (fold_expr p))
   | STRUCT ->
-      append KStruct L  (Path.maptop (fun n -> {n with pad=0}) block.path)
+      let path = reset_line_indent (Region.start_line tok.region) block.path in
+      append KStruct L (Path.maptop (fun n -> {n with pad=0}) path)
   | WHEN ->
       append KWhen L ~pad:(config.i_base + if starts_line then 0 else 2)
         (unwind (function
