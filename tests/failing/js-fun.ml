a8d10f350c7f03b09fa812427de8be22095809a9
Peter Szilagyi <pszilagyi@janestreet.com>
2013-07-02 15:18:06 -0400

    Tweak to the Jane Street indentation of "fun" starting a line.

--- tests/failing/js-fun.ml
+++ tests/failing/js-fun.ml
@@ -69,28 +69,38 @@ let () =
 
 
 
+(* These are based on the observation that a paren before a fun allows the
+   possibility to close the fun and add another statement after the enclosing
+   expression, so the body should be indented.  A fun without parens around it,
+   on the other hand, will include all possible subsequent expressions
+   (including ";"), because "fun ->" binds very loosely. *)
+
 let f =
   f >>= m (fun f ->
     fun x ->
-      y)
+    y);
+  z
 ;;
 
 let f =
   f
   |> m (fun f ->
     fun x ->
-      y)
+    y);
+  z
 ;;
 
 let f =
   f |> m (fun f ->
     (fun x ->
-       y))
+       y));
+  z
 ;;
 
 let f =
   f
   |> m (fun f ->
     (fun x ->
-       y))
+       y));
+  z
 ;;
