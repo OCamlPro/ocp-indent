OCamlPro/ocp-indent

commit e450cb1f76db35ffba28e68b9c55888957fa79b3

Fixed indent of distributive M. after `{ record with` Fixes js-record

src/indentBlock.ml
@@ -1113,7 +1113,7 @@ let rec update_path config block stream tok =
        | {kind=KExpr _} :: {kind=KType} :: ({kind=KColon} :: _ as p) ->
            (* let f: type t. t -> t = ... *)
            p
-       | {kind=KExpr i} :: ({kind=KBrace} as h :: p)
+       | {kind=KExpr i} :: ({kind=KBrace|KWith KBrace} as h :: p)
          when i = prio_max ->
            (* special case: distributive { Module. field; field } *)
            { h with pad = config.i_base } :: p

tests/failing.html
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
 <head>
-    <title>Failing tests, ocp-indent version 1.3.1+2 (2013-07-12)</title>
+    <title>Failing tests, ocp-indent version 1.3.1+3 (2013-07-12)</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <style>
       TABLE { border-collapse: collapse; border-spacing: 0px; margin: auto; }
@@ -16,7 +16,7 @@
     </style>
 </head>
 <body>
-<h1>Failing tests, ocp-indent version 1.3.1+2 (2013-07-12)</h1>
+<h1>Failing tests, ocp-indent version 1.3.1+3 (2013-07-12)</h1>
 <div>
 <h2>Differences in js-args.ml</h2>
 <table>
@@ -248,62 +248,6 @@
 <tr><td class="linenum">44</td><td class="correct"><pre>  Make (M)</pre></td><td class="correct"><pre>  Make (M)</pre></td></tr>
 <tr><td class="linenum">45</td><td class="correct"><pre>module M : S with type t := int =</pre></td><td class="correct"><pre>module M : S with type t := int =</pre></td></tr>
 <tr><td class="linenum">46</td><td class="correct"><pre>  Make (M)</pre></td><td class="correct"><pre>  Make (M)</pre></td></tr>
-</table>
-</div>
-<div>
-<h2>Differences in js-record.ml</h2>
-<table>
-<tr><th><th>Expected<th>Ocp-indent output</tr>
-<tr><td class="linenum">0</td><td class="correct"><pre>type x =</pre></td><td class="correct"><pre>type x =</pre></td></tr>
-<tr><td class="linenum">1</td><td class="correct"><pre>  { foo : int</pre></td><td class="correct"><pre>  { foo : int</pre></td></tr>
-<tr><td class="linenum">2</td><td class="correct"><pre>  ; bar : int</pre></td><td class="correct"><pre>  ; bar : int</pre></td></tr>
-<tr><td class="linenum">3</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">4</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">5</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">6</td><td class="correct"><pre>  { x with</pre></td><td class="correct"><pre>  { x with</pre></td></tr>
-<tr><td class="linenum">7</td><td class="correct"><pre>    foo = 3</pre></td><td class="correct"><pre>    foo = 3</pre></td></tr>
-<tr><td class="linenum">8</td><td class="correct"><pre>  ; bar = 5</pre></td><td class="correct"><pre>  ; bar = 5</pre></td></tr>
-<tr><td class="linenum">9</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">10</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">11</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">12</td><td class="correct"><pre>  { (* blah blah blah *)</pre></td><td class="correct"><pre>  { (* blah blah blah *)</pre></td></tr>
-<tr><td class="linenum">13</td><td class="correct"><pre>    foo = 3</pre></td><td class="correct"><pre>    foo = 3</pre></td></tr>
-<tr><td class="linenum">14</td><td class="correct"><pre>  ; bar = 5</pre></td><td class="correct"><pre>  ; bar = 5</pre></td></tr>
-<tr><td class="linenum">15</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">16</td><td class="correct"><pre>;;</pre></td><td class="correct"><pre>;;</pre></td></tr>
-<tr><td class="linenum">17</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">18</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">19</td><td class="correct"><pre>  [{ x with</pre></td><td class="correct"><pre>  [{ x with</pre></td></tr>
-<tr><td class="linenum">20</td><td class="correct"><pre>     foo = 3</pre></td><td class="correct"><pre>     foo = 3</pre></td></tr>
-<tr><td class="linenum">21</td><td class="correct"><pre>   ; bar = 5</pre></td><td class="correct"><pre>   ; bar = 5</pre></td></tr>
-<tr><td class="linenum">22</td><td class="correct"><pre>   }]</pre></td><td class="correct"><pre>   }]</pre></td></tr>
-<tr><td class="linenum">23</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">24</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">25</td><td class="correct"><pre>  [{ (* blah blah blah *)</pre></td><td class="correct"><pre>  [{ (* blah blah blah *)</pre></td></tr>
-<tr><td class="linenum">26</td><td class="correct"><pre>    foo = 3</pre></td><td class="correct"><pre>    foo = 3</pre></td></tr>
-<tr><td class="linenum">27</td><td class="correct"><pre>  ; bar = 5</pre></td><td class="correct"><pre>  ; bar = 5</pre></td></tr>
-<tr><td class="linenum">28</td><td class="correct"><pre>  }]</pre></td><td class="correct"><pre>  }]</pre></td></tr>
-<tr><td class="linenum">29</td><td class="correct"><pre>;;</pre></td><td class="correct"><pre>;;</pre></td></tr>
-<tr><td class="linenum">30</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">31</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">32</td><td class="correct"><pre>  { M.x with</pre></td><td class="correct"><pre>  { M.x with</pre></td></tr>
-<tr><td class="linenum">33</td><td class="correct"><pre>    M.</pre></td><td class="correct"><pre>    M.</pre></td></tr>
-<tr><td class="linenum">34</td><td class="different"><pre>····foo·=·3</pre></td><td class="different"><pre>······foo·=·3</pre></td></tr>
-<tr><td class="linenum">35</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">36</td><td class="correct"><pre>;;</pre></td><td class="correct"><pre>;;</pre></td></tr>
-<tr><td class="linenum">37</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">38</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">39</td><td class="correct"><pre>  { x with</pre></td><td class="correct"><pre>  { x with</pre></td></tr>
-<tr><td class="linenum">40</td><td class="correct"><pre>    M.</pre></td><td class="correct"><pre>    M.</pre></td></tr>
-<tr><td class="linenum">41</td><td class="different"><pre>····foo·=·3</pre></td><td class="different"><pre>······foo·=·3</pre></td></tr>
-<tr><td class="linenum">42</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">43</td><td class="correct"><pre>;;</pre></td><td class="correct"><pre>;;</pre></td></tr>
-<tr><td class="linenum">44</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">45</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">46</td><td class="correct"><pre>  { M.</pre></td><td class="correct"><pre>  { M.</pre></td></tr>
-<tr><td class="linenum">47</td><td class="correct"><pre>    foo = 3</pre></td><td class="correct"><pre>    foo = 3</pre></td></tr>
-<tr><td class="linenum">48</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
-<tr><td class="linenum">49</td><td class="correct"><pre>;;</pre></td><td class="correct"><pre>;;</pre></td></tr>
 </table>
 </div>
 <div>

tests/failing-output/js-record.ml
@@ -1,50 +1,0 @@
-type x =
-  { foo : int
-  ; bar : int
-  }
-
-let x =
-  { x with
-    foo = 3
-  ; bar = 5
-  }
-
-let x =
-  { (* blah blah blah *)
-    foo = 3
-  ; bar = 5
-  }
-;;
-
-let x =
-  [{ x with
-     foo = 3
-   ; bar = 5
-   }]
-
-let x =
-  [{ (* blah blah blah *)
-    foo = 3
-  ; bar = 5
-  }]
-;;
-
-let x =
-  { M.x with
-    M.
-      foo = 3
-  }
-;;
-
-let x =
-  { x with
-    M.
-      foo = 3
-  }
-;;
-
-let x =
-  { M.
-    foo = 3
-  }
-;;

commit a96eb9f6c45fe1380561679ce713f545a66cd429

Fixed regression on records introduced in e450cb1f76db35ffba28e68b9c55888957fa79b3

src/indentBlock.ml
@@ -1121,7 +1121,7 @@ let rec update_path config block stream tok =
            (* let f: type t. t -> t = ... *)
            p
        | {kind=KExpr i} :: ({kind=KBrace|KWith KBrace} as h :: p)
-         when i = prio_max ->
+         when i = prio_max && next_offset tok stream = None ->
            (* special case: distributive { Module. field; field } *)
            { h with pad = config.i_base } :: p
        | _ -> make_infix tok block.path)

tests/failing.html
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
 <head>
-    <title>Failing tests, ocp-indent version 1.3.2 (2013-08-02)</title>
+    <title>Failing tests, ocp-indent version 1.3.2+3 (2013-08-06)</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <style>
       TABLE { border-collapse: collapse; border-spacing: 0px; margin: auto; }
@@ -16,7 +16,7 @@
     </style>
 </head>
 <body>
-<h1>Failing tests, ocp-indent version 1.3.2 (2013-08-02)</h1>
+<h1>Failing tests, ocp-indent version 1.3.2+3 (2013-08-06)</h1>
 <div>
 <h2>Differences in js-args.ml</h2>
 <table>

tests/passing/record-with.ml
@@ -30,3 +30,6 @@ let b =
     { reczfzrd with a = bli;
                     bzeefe
     }
+
+let lexbuf = { lexbuf with Lexing.lex_start_p = start_pos;
+                           Lexing.lex_curr_p = start_pos; }

tests/passing/records.ml
@@ -29,3 +29,15 @@ let f = function
          f2 = xxx
               = yyy; f3 = Foo,
                           Bar }
+
+let _ =
+  match a with
+  | { kind =
+        x } -> ()
+  | { LibIndex.kind =
+        x } -> ()
+
+let x = { kind =
+            x },
+        { LibIndex.kind =
+            x }