OCamlPro/ocp-indent

commit 03b441e5ba758e2d95caedaeee00fb8e9c469139

Fix indentation after prefix type parameters
happened with polymorphic methods. Fixes #244

src/indentBlock.ml
@@ -1289,6 +1289,9 @@ let rec update_path config block stream tok =
        | Some ({kind=KExpr _} :: {kind=KType} :: ({kind=KColon} :: _ as p)) ->
            (* let f: type t. t -> t = ... *)
            p
+       | Some ({kind=KExpr 200} :: ({kind=KColon} :: _ as p)) ->
+           (* method m : 'x 'y . ... =  *)
+           p
        | Some ({kind=KExpr i} :: ({kind=KBrace|KWith KBrace} as h :: p))
          when (i = prio_max || i = prio_dot) && next_offset tok stream = None ->
            (* special case: distributive { Module. field; field } *)
@@ -1371,6 +1374,8 @@ let rec update_path config block stream tok =
              ) (atom block.path)
        | _ -> atom block.path)
 
+  | LIDENT s when String.length s > 0 && s.[0] = '\'' ->
+      append (KExpr prio_max) L ~pad:0 block.path
   | INT64 _ | INT32 _ | INT _ | LIDENT _
   | FLOAT _ | CHAR _ | STRING _
   | TRUE | FALSE | NATIVEINT _

tests/failing.html
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
 <head>
-    <title>Failing tests, ocp-indent version 1.5.3+16 (2017-03-08)</title>
+    <title>Failing tests, ocp-indent version 1.5.3+18 (2017-03-08)</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <style>
       TABLE { border-collapse: collapse; border-spacing: 0px; margin: auto; }
@@ -16,7 +16,7 @@
     </style>
 </head>
 <body>
-<h1>Failing tests, ocp-indent version 1.5.3+16 (2017-03-08)</h1>
+<h1>Failing tests, ocp-indent version 1.5.3+18 (2017-03-08)</h1>
 <div>
 <h2>Differences in edge-cases.ml</h2>
 <table>
@@ -877,36 +877,6 @@
 <tr><td class="linenum">30</td><td class="correct"><pre>  [ (fun () -></pre></td><td class="correct"><pre>  [ (fun () -></pre></td></tr>
 <tr><td class="linenum">31</td><td class="different"><pre>·······3)·;</pre></td><td class="different"><pre>········3)·;</pre></td></tr>
 <tr><td class="linenum">32</td><td class="correct"><pre>    (fun () -> 4) ]</pre></td><td class="correct"><pre>    (fun () -> 4) ]</pre></td></tr>
-</table>
-</div>
-<div>
-<h2>Differences in object.ml</h2>
-<table>
-<tr><th><th>Expected<th>Ocp-indent output</tr>
-<tr><td class="linenum">0</td><td class="correct"><pre>let x =</pre></td><td class="correct"><pre>let x =</pre></td></tr>
-<tr><td class="linenum">1</td><td class="correct"><pre>  object</pre></td><td class="correct"><pre>  object</pre></td></tr>
-<tr><td class="linenum">2</td><td class="correct"><pre>    inherit foo</pre></td><td class="correct"><pre>    inherit foo</pre></td></tr>
-<tr><td class="linenum">3</td><td class="correct"><pre>    method bar = _</pre></td><td class="correct"><pre>    method bar = _</pre></td></tr>
-<tr><td class="linenum">4</td><td class="correct"><pre>  end</pre></td><td class="correct"><pre>  end</pre></td></tr>
-<tr><td class="linenum">5</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">6</td><td class="correct"><pre>class foo =</pre></td><td class="correct"><pre>class foo =</pre></td></tr>
-<tr><td class="linenum">7</td><td class="correct"><pre>  object</pre></td><td class="correct"><pre>  object</pre></td></tr>
-<tr><td class="linenum">8</td><td class="correct"><pre>    method x = 2</pre></td><td class="correct"><pre>    method x = 2</pre></td></tr>
-<tr><td class="linenum">9</td><td class="correct"><pre>    inherit bar</pre></td><td class="correct"><pre>    inherit bar</pre></td></tr>
-<tr><td class="linenum">10</td><td class="correct"><pre>  end</pre></td><td class="correct"><pre>  end</pre></td></tr>
-<tr><td class="linenum">11</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">12</td><td class="correct"><pre>class foo =</pre></td><td class="correct"><pre>class foo =</pre></td></tr>
-<tr><td class="linenum">13</td><td class="correct"><pre>  object(this)</pre></td><td class="correct"><pre>  object(this)</pre></td></tr>
-<tr><td class="linenum">14</td><td class="correct"><pre>    inherit bar</pre></td><td class="correct"><pre>    inherit bar</pre></td></tr>
-<tr><td class="linenum">15</td><td class="correct"><pre>  end</pre></td><td class="correct"><pre>  end</pre></td></tr>
-<tr><td class="linenum">16</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
-<tr><td class="linenum">17</td><td class="correct"><pre>class virtual map = object</pre></td><td class="correct"><pre>class virtual map = object</pre></td></tr>
-<tr><td class="linenum">18</td><td class="correct"><pre>  method visit_expr_node :</pre></td><td class="correct"><pre>  method visit_expr_node :</pre></td></tr>
-<tr><td class="linenum">19</td><td class="correct"><pre>    'env 'info_0 'info_1 .</pre></td><td class="correct"><pre>    'env 'info_0 'info_1 .</pre></td></tr>
-<tr><td class="linenum">20</td><td class="different"><pre>····('env·->·'info_0·->·'info_1)·-></pre></td><td class="different"><pre>···················('env·->·'info_0·->·'info_1)·-></pre></td></tr>
-<tr><td class="linenum">21</td><td class="correct"><pre>    'env -> 'info_0 expr_node -> 'info_1 expr_node =</pre></td><td class="correct"><pre>    'env -> 'info_0 expr_node -> 'info_1 expr_node =</pre></td></tr>
-<tr><td class="linenum">22</td><td class="correct"><pre>    assert false</pre></td><td class="correct"><pre>    assert false</pre></td></tr>
-<tr><td class="linenum">23</td><td class="correct"><pre>end</pre></td><td class="correct"><pre>end</pre></td></tr>
 </table>
 </div>
 </body>

tests/failing-output/object.ml
@@ -1,24 +1,0 @@
-let x =
-  object
-    inherit foo
-    method bar = _
-  end
-
-class foo =
-  object
-    method x = 2
-    inherit bar
-  end
-
-class foo =
-  object(this)
-    inherit bar
-  end
-
-class virtual map = object
-  method visit_expr_node :
-    'env 'info_0 'info_1 .
-                   ('env -> 'info_0 -> 'info_1) ->
-    'env -> 'info_0 expr_node -> 'info_1 expr_node =
-    assert false
-end

commit d3f250b6029a7afec0d7ddd8770d9c4a7e5b9c7c

Fix regression on record field types
Was caused by 03b441e, which should have been completely unrelated

src/indentBlock.ml
@@ -1291,8 +1291,8 @@ let rec update_path config block stream tok =
        | Some ({kind=KExpr _} :: {kind=KType} :: ({kind=KColon} :: _ as p)) ->
            (* let f: type t. t -> t = ... *)
            p
-       | Some ({kind=KExpr 200} :: ({kind=KColon} :: _ as p)) ->
-           (* method m : 'x 'y . ... =  *)
+       | Some ({kind=KExpr 200} :: ({kind=KColon} :: {kind=KLet} :: _ as p)) ->
+           (* method m : 'x 'y . ... =   (KLet is actually "method") *)
            p
        | Some ({kind=KExpr i} :: ({kind=KBrace|KWith KBrace} as h :: p))
          when (i = prio_max || i = prio_dot) && next_offset tok stream = None ->

tests/failing-output/js-record.ml
@@ -81,7 +81,7 @@ type t =
 type t =
   { a
     : Something_lengthy.t list list
-                          [@default String.Map.empty]
+        [@default String.Map.empty]
   }
 
 type t =
@@ -93,11 +93,11 @@ type t =
 type t =
   { a
     : Something_lengthy.t list
-                          list
+        list
   }
 
 type t =
   { a
     : Something_lengthy.t
-                          list
+        list
   }

tests/failing.html
@@ -2,7 +2,7 @@
  "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
 <head>
-    <title>Failing tests, ocp-indent version 1.6.0+7 (2017-05-04)</title>
+    <title>Failing tests, ocp-indent version 1.6.0+8 (2017-05-04)</title>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <style>
       TABLE { border-collapse: collapse; border-spacing: 0px; margin: auto; }
@@ -17,7 +17,7 @@
     </style>
 </head>
 <body>
-<h1>Failing tests, ocp-indent version 1.6.0+7 (2017-05-04)</h1>
+<h1>Failing tests, ocp-indent version 1.6.0+8 (2017-05-04)</h1>
 <div>
 <h2>Differences in edge-cases.ml</h2>
 <table>
@@ -734,7 +734,7 @@
 <tr><td class="linenum">80</td><td class="correct"><pre>type t =</pre></td><td class="correct"><pre>type t =</pre></td></tr>
 <tr><td class="linenum">81</td><td class="correct"><pre>  { a</pre></td><td class="correct"><pre>  { a</pre></td></tr>
 <tr><td class="linenum">82</td><td class="correct"><pre>    : Something_lengthy.t list list</pre></td><td class="correct"><pre>    : Something_lengthy.t list list</pre></td></tr>
-<tr><td class="linenum">83</td><td class="different"><pre>········[@default·String.Map.empty]</pre></td><td class="different"><pre>··························[@default·String.Map.empty]</pre></td></tr>
+<tr><td class="linenum">83</td><td class="correct"><pre>        [@default String.Map.empty]</pre></td><td class="correct"><pre>        [@default String.Map.empty]</pre></td></tr>
 <tr><td class="linenum">84</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
 <tr><td class="linenum">85</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
 <tr><td class="linenum">86</td><td class="correct"><pre>type t =</pre></td><td class="correct"><pre>type t =</pre></td></tr>
@@ -746,13 +746,13 @@
 <tr><td class="linenum">92</td><td class="correct"><pre>type t =</pre></td><td class="correct"><pre>type t =</pre></td></tr>
 <tr><td class="linenum">93</td><td class="correct"><pre>  { a</pre></td><td class="correct"><pre>  { a</pre></td></tr>
 <tr><td class="linenum">94</td><td class="correct"><pre>    : Something_lengthy.t list</pre></td><td class="correct"><pre>    : Something_lengthy.t list</pre></td></tr>
-<tr><td class="linenum">95</td><td class="different"><pre>········list</pre></td><td class="different"><pre>··························list</pre></td></tr>
+<tr><td class="linenum">95</td><td class="correct"><pre>        list</pre></td><td class="correct"><pre>        list</pre></td></tr>
 <tr><td class="linenum">96</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
 <tr><td class="linenum">97</td><td class="correct"><pre></pre></td><td class="correct"><pre></pre></td></tr>
 <tr><td class="linenum">98</td><td class="correct"><pre>type t =</pre></td><td class="correct"><pre>type t =</pre></td></tr>
 <tr><td class="linenum">99</td><td class="correct"><pre>  { a</pre></td><td class="correct"><pre>  { a</pre></td></tr>
 <tr><td class="linenum">100</td><td class="correct"><pre>    : Something_lengthy.t</pre></td><td class="correct"><pre>    : Something_lengthy.t</pre></td></tr>
-<tr><td class="linenum">101</td><td class="different"><pre>········list</pre></td><td class="different"><pre>··························list</pre></td></tr>
+<tr><td class="linenum">101</td><td class="correct"><pre>        list</pre></td><td class="correct"><pre>        list</pre></td></tr>
 <tr><td class="linenum">102</td><td class="correct"><pre>  }</pre></td><td class="correct"><pre>  }</pre></td></tr>
 </table>
 </div>