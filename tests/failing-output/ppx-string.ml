3ffc9a7c2166f798921b65d26380b89599bc069e
Louis Gesbert <louis.gesbert@ocamlpro.com>
2018-02-01 12:08:56 +0100

    Add failing test for {|strings|}

---
+++ tests/failing-output/ppx-string.ml
@@ -0,0 +1,46 @@
+let s = {|
+|}
+
+let s = {xx|
+xx|}
+
+let s =
+  {xx|
+|}
+|xx}
+
+let s = {| foo
+           bar
+        |}
+
+let s = {| foo
+           bar
+        |}
+
+let s = {| foo
+           bar
+        |}
+
+let s =
+  {|
+|}
+
+let s =
+  {|
+  |}
+
+let s =
+  {| foo
+  |}
+
+let s =
+  {xx| foo
+       bar
+       baz
+  |xx}
+
+let s =
+  {|
+foo bar
+    baz
+       |}
