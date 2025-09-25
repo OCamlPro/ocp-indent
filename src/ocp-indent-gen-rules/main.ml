744f084801bc418d4b001b7144ceabff6016ce6a
Nathan Rebours <nathan.rebours@ocamlpro.com>
2025-09-25 12:03:05 +0200

    Use dynamic_include instead of include for formatting rules
    Signed-off-by: Nathan Rebours <nathan.rebours@ocamlpro.com>

--- src/ocp-indent-gen-rules/main.ml
+++ src/ocp-indent-gen-rules/main.ml
@@ -1,19 +1,37 @@
+let exe_name = "ocp-indent-gen-rules"
+
+let ignore = ref []
+
+let args =
+  [ ( "--ignore"
+    , Arg.String (fun s -> ignore := (String.split_on_char ',' s))
+    , "Comma separated list of files that should not be indented" )
+  ]
+
 let print_rules file =
-  match Filename.extension file with
-  | ".ml" | ".mli" ->
-      let formatted_ext = "fmtd" in
-      let pf fmt = Printf.printf (fmt ^^ "\n") in
-      pf "(rule";
-      pf " (target %s.%s)" file formatted_ext;
-      pf " (action (run ocp-indent %%{dep:%s} -o %%{target})))" file;
-      pf "";
-      pf "(rule";
-      pf " (alias indent)";
-      pf " (action (diff %s %s.%s)))" file file formatted_ext;
-      pf ""
-  | _ -> ()
+  if List.mem file !ignore then
+    ()
+  else
+    match Filename.extension file with
+    | ".ml" | ".mli" ->
+        let formatted_ext = "fmtd" in
+        let pf fmt = Printf.printf (fmt ^^ "\n") in
+        pf "(rule";
+        pf " (target %s.%s)" file formatted_ext;
+        pf " (action (run ocp-indent %%{dep:../%s} -o %%{target})))" file;
+        pf "";
+        pf "(rule";
+        pf " (alias indent)";
+        pf " (action (diff ../%s %s.%s)))" file file formatted_ext;
+        pf ""
+    | _ -> ()
 
 let () =
-  let cwd = Sys.getcwd () in
-  let files = Sys.readdir cwd in
+  let usage = Printf.sprintf "%s [options]" exe_name in
+  let anon_args _ =
+    Printf.eprintf "Usage: %s\n%!" usage;
+    exit 1
+  in
+  Arg.parse args anon_args usage;
+  let files = Sys.readdir ".." in
   Array.iter print_rules files
