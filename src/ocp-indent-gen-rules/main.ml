d40c35e9b1f702d03d092128cb047e52b7e819f3
Nathan Rebours <nathan.rebours@ocamlpro.com>
2025-09-29 15:02:21 +0200

    Make ocp-ident-gen-rules more configurable
    Signed-off-by: Nathan Rebours <nathan.rebours@ocamlpro.com>

--- src/ocp-indent-gen-rules/main.ml
+++ src/ocp-indent-gen-rules/main.ml
@@ -1,41 +1,80 @@
 let exe_name = "ocp-indent-gen-rules"
 
-let ignore = ref []
-let alias = ref "fmt"
+let named f = Cmdliner.Term.(app (const f))
 
-let args =
-  [ ( "--ignore"
-    , Arg.String (fun s -> ignore := (String.split_on_char ',' s))
-    , "Comma separated list of files that should not be indented" )
-  ; ( "--alias"
-    , Arg.Set_string alias
-    , "The alias for the ocp-indent rules, default is fmt" )
-  ]
+let ignore =
+  let open Cmdliner.Arg in
+  let doc = "Comma separated list of files that should not be indented" in
+  named
+    (fun x -> `Ignore x)
+    (value & opt (list string) [] & info ~doc ["ignore"])
 
-let print_rules file =
-  if List.mem file !ignore then
+let alias =
+  let open Cmdliner.Arg in
+  let doc = "The alias for the ocp-indent rules, default is fmt" in
+  named
+    (fun x -> `Alias x)
+    (value & opt string "fmt" & info ~doc ["alias"])
+
+let static =
+  let open Cmdliner.Arg in
+  let doc =
+    "Generate rules for a regular include stanza rather than a dynamic_include"
+  in
+  named
+    (fun x -> `Static x)
+    (value & flag & info ~doc ["static"])
+
+let output =
+  let open Cmdliner.Arg in
+  let docv = "OUTFILE" in
+  let doc = "Writes rules to $(docv). When absent, writes rules to stdout." in
+  named
+    (fun x -> `Output x)
+    (value & opt (some string) None & info ~doc ~docv ["o"])
+
+let print_rules ~ignore ~alias ~static oc file =
+  let source_file = if static then file else "../" ^ file in
+  if List.mem file ignore then
     ()
   else
     match Filename.extension file with
     | ".ml" | ".mli" ->
         let formatted_ext = "fmtd" in
-        let pf fmt = Printf.printf (fmt ^^ "\n") in
+        let pf fmt = Printf.fprintf oc (fmt ^^ "\n") in
         pf "(rule";
         pf " (target %s.%s)" file formatted_ext;
-        pf " (action (run ocp-indent %%{dep:../%s} -o %%{target})))" file;
+        pf " (action (run ocp-indent %%{dep:%s} -o %%{target})))" source_file;
         pf "";
         pf "(rule";
-        pf " (alias %s)" !alias;
-        pf " (action (diff ../%s %s.%s)))" file file formatted_ext;
+        pf " (alias %s)" alias;
+        pf " (action (diff %s %s.%s)))" source_file file formatted_ext;
         pf ""
     | _ -> ()
 
+let with_output ~f output =
+  match output with
+  | None -> f stdout
+  | Some file ->
+      let oc = open_out file in
+      f oc;
+      close_out oc
+
+let run (`Ignore ignore) (`Alias alias) (`Static static)
+    (`Output output) =
+  let src_dir = if static then Sys.getcwd () else ".." in
+  let files = Sys.readdir src_dir in
+  with_output output
+    ~f:(fun oc -> Array.iter (print_rules ~ignore ~alias ~static oc) files)
+
+let term =
+  let open Cmdliner.Term in
+  const run $ ignore $ alias $ static $ output
+
+let info =
+  let open Cmdliner in
+  Cmd.info exe_name ~version:Version.v ~exits:Cmd.Exit.defaults
+    ~doc:"Generate ocp-indent dune rules"
+
 let () =
-  let usage = Printf.sprintf "%s [options]" exe_name in
-  let anon_args _ =
-    Printf.eprintf "Usage: %s\n%!" usage;
-    exit 1
-  in
-  Arg.parse args anon_args usage;
-  let files = Sys.readdir ".." in
-  Array.iter print_rules files
+  exit (Cmdliner.Cmd.eval (Cmdliner.Cmd.v info term))
