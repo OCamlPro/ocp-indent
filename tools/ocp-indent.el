bb04961c1bc0a30ba72247f58bd2dea58d852c02
Bozhidar Batsov <bozhidar@batsov.dev>
2026-03-10 08:59:41 +0200

    Fix typo in ocp-indent.el: "you system" → "your system"

--- tools/ocp-indent.el
+++ tools/ocp-indent.el
@@ -21,7 +21,7 @@
 ;; ocp-indent is a simple tool and library to indent OCaml code.
 
 ;; Installation:
-;; You need ocp-indent installed on you system to work.
+;; You need ocp-indent installed on your system to work.
 
 ;; Usage:
 ;; Eval this file to automatically use ocp-indent on caml/tuareg buffers.
